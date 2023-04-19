7a07a7d99b39e35cce944dacc8629472d637f825
Sebastian Kycia <sebidlo@gmail.com>
2023-04-19 19:09:48 +0200

    adding a function that reads keys

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# create file
+import curses
+
+def main(stdscr):
+    # Ustawiamy kursor konsoli na ukryty
+    curses.curs_set(0)
+    # Ustawiamy nieblokujący tryb klawiatury
+    stdscr.nodelay(True)
+    while True:
+        # Pobieramy kod znaku naciśniętego przez użytkownika
+        key = stdscr.getch()
+        print(key)
+        if key != curses.ERR:
+            # obsługa klawiszy
+            if key == ord('a'):
+                stdscr.addstr(0, 0, "Klawisz 'a' został naciśnięty.")
+            elif key == ord('b'):
+                stdscr.addstr(0, 0, "Klawisz 'b' został naciśnięty.")
+        stdscr.refresh()
+
+# Inicjujemy konsolę
+curses.wrapper(main)
