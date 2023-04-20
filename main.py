746b976d3753868a35d6b3ceba616de89bc75674
Sebastian Kycia <s.kycia@advacom.pl>
2023-04-20 09:09:03 +0200

    test clear screen

--- main.py
+++ main.py
@@ -6,27 +6,31 @@ def clear():
         define our clear function
     '''
     if name == 'nt':
-        _ = system('cls') # for windows
+        _ = system('cls')  # for windows
     else:
-        _ = system('clear') # for mac and linux(here, os.name is 'posix')
+        _ = system('clear')  # for mac and linux(here, os.name is 'posix')
+
 
 def schow_key(key):
     print(f"nacisnieto a ....{key}")
-    
+
+
 def read_key() -> int:
     key_1 = int(input("wybież wartiant "))
     return key_1
+
 
 def main():
     while True:
         key = read_key()
         if key == 1:
+            # clear()
             schow_key(key)
             print("ok 1")
-            clear()
         else:
             print("inny")
             # clear()
-        
+
+
 if __name__ == "__main__":
     main()
